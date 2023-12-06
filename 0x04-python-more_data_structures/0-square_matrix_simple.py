aebe4f4b15cc0c005178809e49de9405f5d4fb1a
root <yousef>
2023-12-06 08:21:00 -0800

    my 1 commit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,9 +3,7 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = [row[:] for row in matrix]
     for idx, row in enumerate(new_matrix):
-        for idx2, col in enumerate(new_matrix):
+        for idx2, _ in enumerate(row):
             new_matrix[idx][idx2] = row[idx2] ** 2
         return (new_matrix)
-
-
 
