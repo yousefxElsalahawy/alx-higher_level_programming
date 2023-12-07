0f000b1ba2f38bd27fcf33554ac907cad6bce859
root <yousef>
2023-12-06 16:01:16 -0800

    my 1 commit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for char in range(97, 123):
-    if (char != 101 and char != 113):
-        print("{:c}".format(char), end='')
+    if (char = 101 or char = 113):
+        continue
+    print("{:c}".format(char), end='')
