efc05b227d054fc078ec4bae62ce5fbe1bfef80e
root <yousef>
2023-12-08 07:16:04 -0800

    my 1 commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-	import sys
-	n = int (len(sys.argv[1:]))
+    import sys
+    i = len(sys.argv) - 1
 
-	if n > 1:
-		print("{} arguments:".format(n))
-		for x in range(1,n + 1):
-			print("{}: {}".format(x, sys.argv[x]))
-	elif n == 0:
-		print("{} arguments.".format(n))
-	elif n == 1:
-		print("{} argument:".format(n))
-		print("{}: {}".format(1, sys.argv[1]))
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
+    else:
+        print("{} arguments:".format(i))
+
+    if i >= 1:
+        i = 0
+        for arg in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, arg))
+            i += 1
