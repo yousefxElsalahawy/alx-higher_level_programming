eb1b9025da7cf41007774b3b8d9b59c8c1d4c0e6
root <yousef>
2023-12-08 07:09:51 -0800

    my 1 commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,4 +11,5 @@ if __name__ == "__main__":
 		print("{} arguments.".format(n))
         else:
                 print("{} argument:".format(n))
-
+                for x in range(1,n + 1):
+                    print("{}: {}".format(x, sys.argv[x]))
