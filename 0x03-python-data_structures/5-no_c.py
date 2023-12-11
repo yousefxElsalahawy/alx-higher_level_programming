7ff155403bc9a648712efe39470813639188d586
root <yousef>
2023-12-11 01:36:21 -0800

    my 1 commit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-
 def no_c(my_string):
-    new_str = ''
-    for char in my_string:
-        if char != 'c' and char != 'C':
-            new_str += char
-    return (new_str)
+    new_string = ""
+    for i in my_string:
+        if i == "c" or i == 'C':
+            continue
+        else:
+            new_string += i
+    return new_string
