cb0efab27b40dd5e74460f87c7497756ddc2e56f
root <yousef>
2023-12-11 01:00:26 -0800

    my 1 commit

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
-
 def new_in_list(my_list, idx, element):
+    if isinstance(my_list, list):
+        list = [x for x in my_list]
 
-    if (idx < 0) or (idx > (len(my_list)-1)):
-        return my_list
+        if idx < 0 or idx >= len(list):
+            return list
+        else:
+            list[idx] = element
+            return list
 
-    copy = [x for x in my_list]
-    copy[idx] = element
-    return copy
+
