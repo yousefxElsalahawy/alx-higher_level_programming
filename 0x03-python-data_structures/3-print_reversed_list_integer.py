8d3b9349ce648a680f61dde5798564bca33a349a
root <yousef>
2023-12-11 00:19:53 -0800

    my 1 commit

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-        for x in range(len(my_list)-1, -1, -1):
-                print("{}".format(my_list[x]))
+    my_list.reverse()
+    for i in my_list:
+        print("{}".format(i))
