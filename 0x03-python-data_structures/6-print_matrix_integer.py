72fe2789dc0e048c0cad36ffe49e6dbc3fdeafcb
root <yousef>
2023-12-11 03:11:48 -0800

    my 1 commit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@
 def print_matrix_integer(matrix=[[]]):
     for x in matrix:
         for j, y in enumerate(x):
-            print("{:d}".format(y), end = " " if j != len(x)-1 else "\n" )
+            print("{:d}".format(y), end = " " if j != len(x)-1 else "$" )
         print("")
