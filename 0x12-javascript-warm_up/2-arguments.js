f1d976c2fe049aec4937ea679aaad445b3141508
You <you@gmail.com>
2024-06-28 15:16:15 +0300

    commit

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-if (process.argv.length < 3) {
+if (process.argv.length < 3 && process.argv.length > 0) {
   console.log("No argument");
 } else if (process.argv.length === 3) {
   console.log("Argument found");
