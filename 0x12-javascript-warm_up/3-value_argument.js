138204e8ecd787b42de82300a09150220aa7fc07
You <you@gmail.com>
2024-06-28 18:23:23 +0300

    commit

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,4 +1,6 @@
-if (process.argv[2] === undefined) {
-  console.log('No argument');
-} else if (process.argv[2] !== undefined) {
-  console.log('process.argv[2]')};
+// if (process.argv[2]) {
+//   console.log('No argument');
+// } else {
+//   console.log('process.argv[2]')};
+
+  console.log(process.argv[2]? process.argv[2] : "No argument");
